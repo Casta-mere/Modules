7a906879ab7238da50b3835c5de3da9c92326082
王旭刚 <castamerego@gmail.com>
2023-06-27 15:23:55 +0800

    添加了Log每日新建文件的功能

--- CODE/Log/log.py
+++ CODE/Log/log.py
@@ -1,13 +1,34 @@
 import time
+import threading
     
 class classlog():
-    def __init__(self,classname):
-        self.date=time.strftime("%Y-%m-%d", time.localtime())
-        self.LogPath=f"{self.date}-{classname}log.log"
-        self.file = open(self.LogPath, 'a', encoding="utf-8")
+
+    def __init__(self,className):
+        self.currentTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        self.currentDate = time.strftime("%Y-%m-%d", time.localtime())
+        self.className = className
+        self.LogPath = f"{self.currentDate}-{self.className}log.log"
+        self.logFile = open(self.LogPath, 'a', encoding="utf-8")
+        self.fileState = True
         self.log("====================================================================")
         self.log("NEW INSTANCE RUNNING")
+
+        # Start thread for checking date
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
     
+    def run(self):
+        while True:
+            # Create new log file every day
+            if(self.currentDate != time.strftime("%Y-%m-%d", time.localtime())):
+                self.fileState = False
+                self.currentDate = time.strftime("%Y-%m-%d", time.localtime())
+                self.logFile.close()
+                self.LogPath = f"{self.currentDate}-{self.className}log.log"
+                self.logFile = open(self.LogPath, 'a', encoding="utf-8")
+                self.fileState = True
+
     def log(self,msg):
-        self.file.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " " + msg +"\n")
-        self.file.flush()
+        if(self.fileState):
+            self.logFile.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " " + msg +"\n")
+            self.logFile.flush()
